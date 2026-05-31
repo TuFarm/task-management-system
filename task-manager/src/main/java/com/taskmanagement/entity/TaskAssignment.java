f283900d7c10a3c3e43f8aef02a5103fc855ba0d
TuFarm <kpnkt.vn@gmail.com>
2026-05-31 23:39:08 +0700

    fix: TaskAssignment equals/hashCode dùng taskId+userId thay vì assignmentId null

--- task-manager/src/main/java/com/taskmanagement/entity/TaskAssignment.java
+++ task-manager/src/main/java/com/taskmanagement/entity/TaskAssignment.java
@@ -5,10 +5,10 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "task_assignments")
@@ -16,13 +16,11 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class TaskAssignment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "assignment_id")
-    @EqualsAndHashCode.Include
     private Integer assignmentId;
 
     @ManyToOne
@@ -36,4 +34,21 @@ public class TaskAssignment {
 
     @Column(name = "assigned_at")
     private LocalDateTime assignedAt = LocalDateTime.now();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TaskAssignment other)) return false;
+        if (task == null || user == null || other.task == null || other.user == null) return false;
+        return Objects.equals(task.getTaskId(), other.task.getTaskId())
+            && Objects.equals(user.getUserId(), other.user.getUserId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+            task != null ? task.getTaskId() : null,
+            user != null ? user.getUserId() : null
+        );
+    }
 }
